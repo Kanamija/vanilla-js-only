71902061f1f01d6aca59b928efd862311629bee3
Kanami Anderson <kanamianderson@gmail.com>
2025-12-08 14:17:33 -0600

    Practiced the Map Constructor.

--- vanilla-js-practice.js
+++ vanilla-js-practice.js
@@ -13,4 +13,9 @@
 
 // console.log(mySet);
 
-const map = newMap(['a, 1'], ['b', 2])
+const map = new Map([['a', '1'], ['b', 2]]);
+map.set('c', 3);
+
+for (const [key, value] of map) {
+    console.log(`${key}: ${value}`);
+}
