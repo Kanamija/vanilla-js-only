770535efb752520327f165a12ba554ff25eb995a
Kanami Anderson <kanamianderson@gmail.com>
2025-12-08 14:33:57 -0600

    Learning queue data structures.

--- vanilla-js-practice.js
+++ vanilla-js-practice.js
@@ -1,21 +1,37 @@
-// const set = new Set([1, 2, 3]);
-// set.add(4);
-
-// for (const item of set) {
-//     console.log(item);
-// }
-
-// const mySet = new Set();
-// mySet.add(1);
-// mySet.add('hello');
-// mySet.add({ name: 'Kanami' });
-// mySet.add([1, 2, 3]);
-
-// console.log(mySet);
-
-const map = new Map([['a', '1'], ['b', 2]]);
-map.set('c', 3);
-
-for (const [key, value] of map) {
-    console.log(`${key}: ${value}`);
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    enqueue(element) {
+        this.items.push(element);
+    }
+    dequeue() {
+        return this.items.shift();
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    peek() {
+        if(!this.isEmpty()) {
+            return this.items[0];
+        }
+        return null;
+    }
+    size() {
+        return this.items.length;
+    }
+    print() {
+        console.log(this.items.toString())
+    }
 }
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.size());
+queue.print();
+console.log(queue.dequeue());
+console.log(queue.peek());
