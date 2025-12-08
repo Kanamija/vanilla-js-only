8de256943752bbb0614f1e005329c04237ad3845
Kanami Anderson <kanamianderson@gmail.com>
2025-12-08 13:59:08 -0600

    Putting this Vanilla JS file into its own folder.

---
+++ vanilla-js-practice.js
@@ -0,0 +1,16 @@
+// const set = new Set([1, 2, 3]);
+// set.add(4);
+
+// for (const item of set) {
+//     console.log(item);
+// }
+
+// const mySet = new Set();
+// mySet.add(1);
+// mySet.add('hello');
+// mySet.add({ name: 'Kanami' });
+// mySet.add([1, 2, 3]);
+
+// console.log(mySet);
+
+const map = newMap(['a, 1'], ['b', 2])
