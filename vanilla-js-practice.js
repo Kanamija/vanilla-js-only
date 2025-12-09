3b8b23c0a8efa2c5238b4e2a91cb35312125632f
Kanami Anderson <kanamianderson@gmail.com>
2025-12-09 11:36:20 -0600

    Just finished lesson on Circular Queues. Not sure if I will ever use this. It seems overly complicated for

--- vanilla-js-practice.js
+++ vanilla-js-practice.js
@@ -1,46 +1,71 @@
-class Queue {
-    constructor() {
-        this.items = {};
-        this.rear = 0;
-        this.front = 0;
+class CircularQueue {
+    constructor(capacity) {
+        this.items = new Array(capacity);
+        this.capacity = capacity;
+        this.currentLength = 0;
+        this.rear = -1;
+        this.front = -1;
     }
-    enqueue(element) {
-        this.items[this.rear] = element;
-        this.rear++;
-    }
-    dequeue() {
-        const item = this.items[this.front];
-        delete this.items[this.front];
-        this.front++;
-        return item;
+    isFull() {
+        return this.currentLength === this.capacity;
     }
     isEmpty() {
-        return this.rear - this.front === 0;
+        return this.currentLength === 0;
+    }
+    enqueue(element) {
+        if(!this.isFull()) {
+            this.rear = (this.rear++) % this.capacity;
+            this.items[this.rear] = element;
+            this.currentLength++;
+            if (this.front === -1) {
+                this.front = this.rear;
+            }
+        }
+
+    }
+    dequeue() {
+        if(this.isEmpty()) {
+            return null
+        } 
+        const item = this.items[this.front];
+        this.items[this.front] = null;
+        this.front = (this.front++) % this.capacity;
+        this.currentLength--
+        if(this.isEmpty()) {
+            this.front = -1;
+            this.rear = -1;
+        }
+        return item;
     }
     peek() {
-        return this.items[this.front]
-    }
-    size() {
-        return this.rear - this.front;
+        if(!this.isEmpty()) {
+            return this.items[this.front];
+        }
+        return null;
     }
     print() {
-        console.log(this.items);
+        if(this.isEmpty()) {
+            console.log('Queue is empty');
+        } else {
+            let i; 
+            let str = '';
+            for (i = this.front; i !== this.rear; i = (i +1) % this.capacity) {
+                str += this.items[i] + " "
+            }
+            str += this.items[i];
+            console.log(str);
+        }
     }
 }
 
-const queue = new Queue();
-console.log(queue.isEmpty())
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
-console.log(queue.size());
-queue.print();
+const queue = new CircularQueue(5);
+console.log(queue.isEmpty());
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+queue.enqueue(40)
+queue.enqueue(50)
+console.log(queue.isFull())
+queue.print()
 console.log(queue.dequeue());
-queue.print();
-queue.enqueue(40);
-queue.enqueue(50);
-queue.enqueue(60);
-console.log(queue.dequeue());
-console.log(queue.dequeue());
-queue.peek();
-queue.print();
+console.log(queue.peek());
