44c29be592434d78c52e98c0886da685b42ad301
Kanami Anderson <kanamianderson@gmail.com>
2026-01-10 14:27:09 -0600

    Last update on majority code.

--- group-study-practice.js
+++ group-study-practice.js
@@ -7,24 +7,24 @@
 // majority should return false.
 
 
-// function majority(array, callback) {
-//     let trues = 0
-//     let falses = 0
-//     for (let i = 0; i < array.length; i++) {
-//         if(callback(array[i])) {
-//             trues++
-//         } else {
-//             falses++
-//         }
-//     }
-//    if (falses >= trues) return false;
-//    return true;
+function majority(array, callback) {
+    let trues = 0
+    let falses = 0
+    for (let i = 0; i < array.length; i++) {
+        if(callback(array[i])) {
+            trues++
+        } else {
+            falses++
+        }
+    }
+   if (falses >= trues) return false;
+   return true;
     
-// }
+}
 
 // // /*** Uncomment these to check your work! ***/
-// const isOdd = function(num) { return num % 2 === 1; };
-// console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
-// console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
+const isOdd = function(num) { return num % 2 === 1; };
+console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
 
 
